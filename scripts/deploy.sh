c3d6dadcdb847d4dd32cff732f2e116247240b2d
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2020-06-04 14:57:35 +0900

    S3 + CodeDeploy 연동

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,7 +18,7 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/$PROJECT_NAME/build/libs/*jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl freelec-springboot2-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl demo | grep jar | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션pid: $CURRENT_PID"
 if [ -z "$CURRENT_PID" ]; then
