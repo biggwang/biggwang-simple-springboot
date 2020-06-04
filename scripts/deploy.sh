a9e784b554664d8be02395c569a8fe9626e9840b
류윤광/플랫폼개바팀 <yungwang.ryu@musinsa.com>
2020-06-04 23:00:56 +0900

    S3 + CodeDeploy 연동

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,9 +18,9 @@ echo "> Build 파일 복사"
 cp $REPOSITORY/$PROJECT_NAME/build/libs/*jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -fl demo | grep jar | awk '{print $1}')
-
+CURRENT_PID=$(pgrep -f demo)
 echo "> 현재 구동 중인 애플리케이션pid: $CURRENT_PID"
+
 if [ -z "$CURRENT_PID" ]; then
     echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
